f334203fe512cefbdcd4c717fe2246363f479db5
rongbin.wang <rongbin.wang@dmall.com>
2020-08-10 15:00:28 +0800

    Initial commit

--- buckets_manager/comm/authentications.py
+++ buckets_manager/comm/authentications.py
@@ -2,28 +2,28 @@
 # -*- coding: utf-8 -*-
 from rest_framework import exceptions
 from rest_framework.authentication import BaseAuthentication, BasicAuthentication, TokenAuthentication
+import requests
+
+_SSO_URL = getattr('settings', 'SSO_URL', None)
 
 
 class SsoAuthentication(BaseAuthentication):
-    """sso custorm realm
-       to be continue
+    """前端进行sso认证登录，此处只需验证token有效性
     """
     def authenticate(self, request):
-        pass
+        auth_header = request.Meta.get("Authorization")
+        if auth_header:
+            try:
+                headers = {"Authorization": auth_header}
+                req = requests.get(url=_SSO_URL, headers=headers)
+                pass
+            except exceptions.AuthenticationFailed('test'):
+                pass
 
     def authenticate_header(self, request):
         pass
 
 
-class SsoBasicAuthentication(BasicAuthentication):
-    """ override authenticate_credentials()
-    """
-    def authenticate_credentials(self, userid, password, request=None):
-        """改写成通过SSO认证"""
-        pass
-
-
 class SsoTokenAuthentication(TokenAuthentication):
     """sso token realm
-       recommend
     """
